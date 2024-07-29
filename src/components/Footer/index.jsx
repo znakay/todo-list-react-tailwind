ed1960a891870eb0b7dbcb722c8e7d38c9ef367b
Andrey Znak <andreyznak@estr.co>
2024-07-29 17:20:16 +0300

    feat: add styles for #root

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="bg-black">
+  <footer className="bg-black mt-auto py-[20px]">
     <div className="container">
       <span className="text-white text-xl">
         Created by{" "}
-        <a href="https://github.com/znakay" className="hover:text-sky transition-color duration-300">
+        <a href="https://github.com/znakay" target="_blank" className="hover:text-sky transition-color duration-300">
           @znakay
         </a>
       </span>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => (
-  <header className="bg-sky">
+  <header className="bg-sky py-[10px]">
     <div className="container">
       <h1 className="font-sans text-h1-mobile lg:text-h1 font-bold cursor-pointer hover:opacity-50 transition-opacity duration-300 w-fit">
         <a href="#" className="text-white">Todos with TailwindCss :D</a>
