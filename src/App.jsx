b5e07157e649d8feb82bd8b7a1ac3b69462f74dc
Andrey Znak <andreyznak@estr.co>
2024-07-29 17:01:02 +0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
-import "./App.css";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-black">
+    <div className="container">
+      <span className="text-white text-xl">
+        Created by{" "}
+        <a href="https://github.com/znakay" className="hover:text-sky transition-color duration-300">
+          @znakay
+        </a>
+      </span>
+    </div>
+  </footer>
+);
+
+export default Footer;
